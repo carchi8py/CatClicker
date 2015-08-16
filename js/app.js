1f2e1635a703845a9d276f0cfbd6d17f2bec7025
Chris Archibald <carchi8py@gmail.com>
2015-08-15 23:05:22 -0700

    2 cats now are clickable and increment independently of each other

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 
-$("#cat-img").click(function(e) {
-	var count = $('#cat-count').text();
-	count = parseInt(count) + 1;
-	$('#cat-count').text(count);
-})
+var cats = $(".cat");
+
+function bindCounterToCat(idNumber) {
+	var cat = "#cat" + idNumber;
+	console.log(cat)
+	$(cat).click(function(e) {
+		var count = $(cat + " > .counter").text();
+		count = parseInt(count) + 1;
+		$(cat + " > .counter").text(count);
+	})
+}
+
+
+for (var i=1; i<=cats.length; i++) {
+	bindCounterToCat(i)
+}
