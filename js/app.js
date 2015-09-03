a9283cc06cb101917f40982022271b58474d1fce
Chris Archibald <carchi8py@gmail.com>
2015-09-02 21:30:09 -0700

    Modified code to add mutiple cats

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
+var initalCats = [
+		{
+			clickCount : 0,
+			name : "Tabby",
+			imgSrc : "img/434164568_fea0ad4013_z.jpg",
+			imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+			nicknames: ['Tabtab', 'T-bone', 'Mr. T']
+        },
+        {
+            clickCount : 0,
+            name : 'Tiger',
+            imgSrc : "img/4154543904_6e2428c421_z.jpg",
+            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+            nicknames: ['Tigger']
+        },
+        {
+            clickCount : 0,
+            name : 'Scaredy',
+            imgSrc : "img/22252709_010df3379e_z.jpg",
+            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+            nicknames: ['Casper']
+        },
+        {
+            clickCount : 0,
+            name : 'Shadow',
+            imgSrc : "img/1413379559_412a540d29_z.jpg",
+            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+            nicknames: ['shooby']
+        },
+        {
+            clickCount : 0,
+            name : 'Sleepy',
+            imgSrc : "img/9648464288_2516b35537_z.jpg",
+            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+            nicknames: ['Zzzzzz']
+        }
+    ];
+
 var Cat = function(data) {
 	this.clickCount = ko.observable(data.clickCount);
 	this.name = ko.observable(data.name);
@@ -30,13 +68,13 @@ var ViewModel = function() {
 	//I am not using this, but i want to keep it here so i know
 	var self = this;
 
-	this.currentCat = ko.observable( new Cat({
-		clickCount: 0,
-		name: 'Tabby',
-		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-		imgAttribution: 'Nope',
-		nicknames: ['Tabtab', 'T-bone', 'Mr. T']
-	}) );
+	this.catList = ko.observableArray([]);
+
+	initalCats.forEach(function(catItem){
+		self.catList.push( new Cat(catItem));
+	});
+
+	this.currentCat = ko.observable( this.catList()[0] );
 
 	//Because we are using with in the HTML this is now inside the CAT binding
 	//context and not the viewModel
